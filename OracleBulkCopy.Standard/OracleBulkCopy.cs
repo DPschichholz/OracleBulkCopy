1bd550c033aa542802a3534ea70860a13d49c659
Pschichholz <david.pschichholz@dedalus.eu>
2026-05-06 13:53:45 +0200

    Refactor OracleBulkCopy class for improved structure and async support

--- OracleBulkCopy.Standard/OracleBulkCopy.cs
+++ OracleBulkCopy.Standard/OracleBulkCopy.cs
@@ -1,303 +1,289 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
 
-namespace Oracle.ManagedDataAccess.Client.BulkCopy
+namespace OracleBulkCopy.Standard;
+
+public sealed class OracleBulkCopy : IDisposable, IAsyncDisposable
 {
-    public class OracleBulkCopy : IDisposable
-    {
-        // https://github.com/Microsoft/referencesource/blob/master/System.Data/System/Data/SqlClient/SqlBulkCopy.cs
-        // https://stackoverflow.com/questions/47942691/how-to-make-a-bulk-insert-using-oracle-managed-data-acess-c-sharp
-        // https://github.com/DigitalPlatform/dp2/blob/master/DigitalPlatform.rms.db/OracleBulkCopy.cs
-        // https://msdn.microsoft.com/en-us/library/system.data.oracleclient.oracletype(v=vs.110).aspx
-
-        private OracleConnection _connection;
-        private OracleTransaction _externalTransaction { get; set; }
-
-        /// <summary>
-        /// Set to TRUE if the BulkCopy object was not instantiated with an external OracleConnection
-        /// and thus it is up to the BulkCopy object to open and close connections
-        /// </summary>
-        private bool _ownsTheConnection = false;
-
-        public OracleBulkCopy(string connectionString) : this(new OracleConnection(connectionString))
-        {
-            _ownsTheConnection = true;
-        }
-
-        public OracleBulkCopy(OracleConnection connection) : this(connection, null) { }
-
-        public OracleBulkCopy(OracleConnection connection, OracleTransaction transation = null)
-        {
-            _connection = connection;
-            _externalTransaction = transation;
-        }
-
-
-        private string _destinationTableName;
-        
-        public string DestinationTableName {
-            get { return _destinationTableName; }
-            set
-            {
-                if (value == null || value.Length == 0)
-                    throw new ArgumentException("Destination Table Name cannot be null or empty string");
-                _destinationTableName = value;
-            }
-        }
-
-
-        private int _batchSize = 0;
-
-        public int BatchSize {
-            get { return _batchSize; }
-            set {
-                if (value < 0)
-                    throw new ArgumentException("Batch Size must be a positive integer");
-                _batchSize = value;
-            }
-        }
-
-        private bool UploadEverythingInSingleBatch { get { return _batchSize == 0; } }
-
-
-
-
-        private void ValidateConnection()
-        {
-            if (_connection == null)
-                throw new Exception("Oracle Database Connection is required");
-
-            if (_externalTransaction != null && _externalTransaction.Connection != _connection)
-                throw new Exception("Oracle Transaction mismatch with Oracle Database Connection");
-        }
-
-        private void OpenConnection()
-        {
-            if (this._ownsTheConnection && _connection.State != ConnectionState.Open)
-                _connection.Open();
-        }
-
-
-        // TODO: Implement WriteToServer for a IDataReader input
-
-
-        public void WriteToServer(DataTable table)
-        {
-            // https://stackoverflow.com/questions/47942691/how-to-make-a-bulk-insert-using-oracle-managed-data-acess-c-sharp
-            // https://github.com/Microsoft/referencesource/blob/master/System.Data/System/Data/SqlClient/SqlBulkCopy.cs
-
-            if (table == null)
-                throw new ArgumentNullException("table");
-
-            // TODO: Validate TableName to prevent SQL Injection
-            // https://oracle-base.com/articles/10g/dbms_assert_10gR2
-            // SELECT SYS.DBMS_ASSERT.qualified_sql_name('object_name') FROM dual;
-
-            if (UploadEverythingInSingleBatch)
-                WriteToServerInSingleBatch(table);
-            else
-                WriteToServerInMultipleBatches(table);
-        }
-
-        private void WriteToServerInSingleBatch(DataTable table)
-        {
-            // Build the command string
-            string commandText = BuildCommandText(table);
-
-            WriteSingleBatchOfData(table, 0, commandText, table.Rows.Count);
-        }
-
-
-        private void WriteToServerInMultipleBatches(DataTable table)
-        {
-            // Calculate number of batches
-            int numBatchesRequired = (int)Math.Ceiling(table.Rows.Count / (double)BatchSize);
-
-            // Build the command string
-            string commandText = BuildCommandText(table);
-
-            for (int i = 0; i < numBatchesRequired; i++)
-            {
-                int skipOffset = i * BatchSize;
-                int batchSize = Math.Min(BatchSize, table.Rows.Count - skipOffset);
-                WriteSingleBatchOfData(table, skipOffset, commandText, batchSize);
-            }
-        }
-
-        private string BuildCommandText(DataTable table)
-        {
-            // Build the command string
-            string commandText = "Insert Into " + DestinationTableName + " ( @@ColumnList@@ ) Values ( @@ValueList@@ )";
-            string columnList = GetColumnList(table);
-            string valueList = GetValueList(table);
-
-            // Replace the placeholders with actual values
-            commandText = commandText.Replace("@@ColumnList@@", columnList);
-            commandText = commandText.Replace("@@ValueList@@", valueList);
-
-            // TODO: Validate commandText to prevent SQL Injection
-            // https://oracle-base.com/articles/10g/dbms_assert_10gR2
-
-            return commandText;
-        }
-
-
-        private void WriteSingleBatchOfData(DataTable table, int skipOffset, string commandText, int batchSize)
-        {
-            // Get array of row data for all columns in the table
-            List<OracleParameter> parameters = GetParameters(table, batchSize, skipOffset);
-
-            // Create the OracleCommand and bind the data
-            OracleCommand cmd = _connection.CreateCommand();
-            cmd.CommandText = commandText;
-            cmd.ArrayBindCount = batchSize;
-            parameters.ForEach(p => cmd.Parameters.Add(p));
-
-            // Validate and open the connection
-            ValidateConnection();
-            OpenConnection();
-
-            // Upload the data
-            cmd.ExecuteNonQuery();
-
-            // Commit Transaction
-            //CommitTransaction(); // ????
-        }
-
-
-
-
-        private List<OracleParameter> GetParameters(DataTable data, int batchSize, int skipOffset = 0)
-        {
-
-            List<OracleParameter> parameters = new List<OracleParameter>();
-            foreach (DataColumn c in data.Columns)
-            {
-
-                OracleDbType dbType = GetOracleDbTypeFromDotnetType(c.DataType);
-
-                // https://stackoverflow.com/a/23735845/2442468
-                // https://stackoverflow.com/a/17595403/2442468
-
-                var columnData = data.AsEnumerable().Select(r => r.Field<object>(c.ColumnName));
-                object[] paramDataArray = (UploadEverythingInSingleBatch)
-                    ? columnData.ToArray()
-                    : columnData.Skip(skipOffset).Take(batchSize).ToArray();
-
-                OracleParameter param = new OracleParameter();
-                param.OracleDbType = dbType;
-                param.Value = paramDataArray;
-
-                parameters.Add(param);
-            }
-
-            return parameters;
-        }
-
-
-
-
-        private string GetColumnList(DataTable data)
-        {
-            string[] columnNames = data.Columns.Cast<DataColumn>().Select(x => x.ColumnName).ToArray();
-            string columnList = string.Join(",", columnNames);
-            return columnList;
-        }
-
-        private string GetValueList(DataTable data)
-        {
-            const string Delimiter = ", ";
-            
-            StringBuilder sb = new StringBuilder();
-            for (int i = 1; i <= data.Columns.Count; i++)
-            {
-                sb.Append(string.Format(":{0}", i));
-                sb.Append(Delimiter);
-            }
-
-            sb.Length -= Delimiter.Length;
-
-            string valueList = sb.ToString();
-            return valueList;
-        }
-
-
-
-
-        public void Dispose()
-        {
-            if (_connection != null)
-            {
-                // Only close the connection if the BulkCopy instance owns the connection
-                if(this._ownsTheConnection)
-                    _connection.Dispose();
-                
-                // Always set to null
-                _connection = null;
-            }
-        }
-
-        public void Close() { Dispose(); }
-
-
-        private static OracleDbType GetOracleDbType(object o) 
-        {
-            // https://stackoverflow.com/questions/1583150/c-oracle-data-type-equivalence-with-oracledbtype#1583197
-            // https://docs.oracle.com/cd/B19306_01/win.102/b14307/OracleDbTypeEnumerationType.htm
-            // https://docs.microsoft.com/en-us/dotnet/framework/data/adonet/oracle-data-type-mappings
-
-            if (o is byte[]) return OracleDbType.Blob;
-            if (o is string) return OracleDbType.Varchar2;
-            if (o is DateTime) return OracleDbType.Date;
-            if (o is decimal) return OracleDbType.Decimal;
-            if (o is Int32) return OracleDbType.Int32;
-
-            if (o is Int64) return OracleDbType.Int64;
-            if (o is Int16) return OracleDbType.Int16;
-            if (o is sbyte) return OracleDbType.Byte;
-            if (o is byte) return OracleDbType.Int16;    // <== unverified
-            if (o is float) return OracleDbType.Single;
-            if (o is double) return OracleDbType.Double;
-
-
-            // Tylers
-            //if (o is bool) return OracleDbType.Boolean;
-            //if (o is char) return OracleDbType.Char;
-
-
-            return OracleDbType.Varchar2;
-        }
-
-        private static OracleDbType GetOracleDbTypeFromDotnetType(Type t)
-        {
-
-            if (t == typeof(byte[])) return OracleDbType.Blob;
-            if (t == typeof(string)) return OracleDbType.Varchar2;
-            if (t == typeof(DateTime)) return OracleDbType.Date;
-            if (t == typeof(decimal)) return OracleDbType.Decimal;
-            if (t == typeof(Int32)) return OracleDbType.Int32;
-
-            if (t == typeof(Int64)) return OracleDbType.Int64;
-            if (t == typeof(Int16)) return OracleDbType.Int16;
-            if (t == typeof(sbyte)) return OracleDbType.Byte;
-            if (t == typeof(byte)) return OracleDbType.Int16;    // <== unverified
-            if (t == typeof(float)) return OracleDbType.Single;
-            if (t == typeof(double)) return OracleDbType.Double;
-
-
-            // Tylers
-            //if (o is bool) return OracleDbType.Boolean;
-            //if (o is char) return OracleDbType.Char;
-
-
-            return OracleDbType.Varchar2;
-        }
-
-
-
-    }
+	// https://github.com/Microsoft/referencesource/blob/master/System.Data/System/Data/SqlClient/SqlBulkCopy.cs
+	// https://stackoverflow.com/questions/47942691/how-to-make-a-bulk-insert-using-oracle-managed-data-acess-c-sharp
+	// https://github.com/DigitalPlatform/dp2/blob/master/DigitalPlatform.rms.db/OracleBulkCopy.cs
+	// https://msdn.microsoft.com/en-us/library/system.data.oracleclient.oracletype(v=vs.110).aspx
+
+	private readonly OracleConnection _connection;
+	private readonly OracleTransaction? _externalTransaction;
+
+	/// <summary>
+	///     Set to true if the BulkCopy object instantiated its own OracleConnection.
+	/// </summary>
+	private readonly bool _ownsTheConnection;
+
+	private int _batchSize;
+	private bool _disposed;
+
+	public OracleBulkCopy(string connectionString)
+		: this(new OracleConnection(connectionString), null, true)
+	{
+	}
+
+	public OracleBulkCopy(OracleConnection connection, OracleTransaction? transaction = null)
+		: this(connection, transaction, false)
+	{
+	}
+
+	private OracleBulkCopy(OracleConnection connection, OracleTransaction? transaction, bool ownsTheConnection)
+	{
+		ArgumentNullException.ThrowIfNull(connection);
+
+		_connection = connection;
+		_externalTransaction = transaction;
+		_ownsTheConnection = ownsTheConnection;
+	}
+
+	public required string DestinationTableName
+	{
+		get;
+		init
+		{
+			if (string.IsNullOrWhiteSpace(value))
+				throw new InvalidOperationException("DestinationTableName must be set before calling WriteToServer.");
+
+			field = value;
+		}
+	}
+
+	public int BatchSize
+	{
+		get => _batchSize;
+		set
+		{
+			if (value < 0) throw new ArgumentOutOfRangeException(nameof(value), "BatchSize must be >= 0.");
+
+			_batchSize = value;
+		}
+	}
+
+	private bool UploadEverythingInSingleBatch => _batchSize is 0;
+
+	public async ValueTask DisposeAsync()
+	{
+		if (_disposed) return;
+
+		if (_ownsTheConnection) await _connection.DisposeAsync().ConfigureAwait(false);
+
+		_disposed = true;
+		GC.SuppressFinalize(this);
+	}
+
+	public void Dispose()
+	{
+		if (_disposed) return;
+
+		if (_ownsTheConnection) _connection.Dispose();
+
+		_disposed = true;
+		GC.SuppressFinalize(this);
+	}
+
+	public void WriteToServer(DataTable table)
+	{
+		ThrowIfDisposed();
+		ArgumentNullException.ThrowIfNull(table);
+
+		if (table.Columns.Count is 0)
+			throw new ArgumentException("DataTable must contain at least one column.", nameof(table));
+
+		if (table.Rows.Count is 0) return;
+
+		ValidateConnection();
+		OpenConnection();
+
+		if (UploadEverythingInSingleBatch)
+			WriteToServerInSingleBatch(table);
+		else
+			WriteToServerInMultipleBatches(table);
+	}
+
+	private void WriteToServerInSingleBatch(DataTable table)
+	{
+		var commandText = BuildCommandText(table);
+		WriteSingleBatchOfData(table, 0, commandText, table.Rows.Count);
+	}
+
+	private void WriteToServerInMultipleBatches(DataTable table)
+	{
+		var commandText = BuildCommandText(table);
+
+		for (var skipOffset = 0; skipOffset < table.Rows.Count; skipOffset += BatchSize)
+		{
+			var currentBatchSize = Math.Min(BatchSize, table.Rows.Count - skipOffset);
+			WriteSingleBatchOfData(table, skipOffset, commandText, currentBatchSize);
+		}
+	}
+
+	private string BuildCommandText(DataTable table)
+	{
+		var safeTableName = QuoteQualifiedName(DestinationTableName);
+		var columnList = GetColumnList(table);
+		var valueList = GetValueList(table.Columns.Count);
+
+		return $"INSERT INTO {safeTableName} ({columnList}) VALUES ({valueList})";
+	}
+
+	private void WriteSingleBatchOfData(DataTable table, int skipOffset, string commandText, int batchSize)
+	{
+		var parameters = GetParameters(table, batchSize, skipOffset);
+
+		using var cmd = _connection.CreateCommand();
+		// cmd.BindByName = true;
+		cmd.CommandText = commandText;
+		cmd.ArrayBindCount = batchSize;
+
+		if (_externalTransaction is not null) cmd.Transaction = _externalTransaction;
+
+		foreach (var parameter in parameters) cmd.Parameters.Add(parameter);
+
+		cmd.ExecuteNonQuery();
+	}
+
+	private static List<OracleParameter> GetParameters(DataTable data, int batchSize, int skipOffset)
+	{
+		var parameters = new List<OracleParameter>(data.Columns.Count);
+
+		for (var colIndex = 0; colIndex < data.Columns.Count; colIndex++)
+		{
+			var column = data.Columns[colIndex];
+			var dbType = GetOracleDbTypeFromDotnetType(column.DataType);
+			var values = new object[batchSize];
+
+			for (var rowIndex = 0; rowIndex < batchSize; rowIndex++)
+			{
+				var sourceValue = data.Rows[skipOffset + rowIndex][colIndex];
+				values[rowIndex] = sourceValue is null || sourceValue == DBNull.Value ? DBNull.Value : sourceValue;
+			}
+
+			parameters.Add(new OracleParameter($":{colIndex + 1}", dbType)
+			{
+				Value = values
+			});
+		}
+
+		return parameters;
+	}
+
+	private static string GetColumnList(DataTable data)
+	{
+		var sb = new StringBuilder(data.Columns.Count * 16);
+
+		for (var i = 0; i < data.Columns.Count; i++)
+		{
+			if (i > 0) sb.Append(", ");
+
+			sb.Append(QuoteIdentifier(data.Columns[i].ColumnName));
+		}
+
+		return sb.ToString();
+	}
+
+	private static string GetValueList(int columnCount)
+	{
+		var sb = new StringBuilder(columnCount * 5);
+
+		for (var i = 1; i <= columnCount; i++)
+		{
+			if (i > 1) sb.Append(", ");
+
+			sb.Append(':');
+			sb.Append(i);
+		}
+
+		return sb.ToString();
+	}
+
+	private static string QuoteQualifiedName(string qualifiedName)
+	{
+		if (string.IsNullOrWhiteSpace(qualifiedName))
+			throw new ArgumentException("Object name cannot be null or empty.", nameof(qualifiedName));
+
+		var parts = qualifiedName.Split(new[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
+		if (parts.Length is 0) throw new ArgumentException("Object name is invalid.", nameof(qualifiedName));
+
+		var sb = new StringBuilder(qualifiedName.Length + 4);
+		for (var i = 0; i < parts.Length; i++)
+		{
+			if (i > 0) sb.Append('.');
+
+			sb.Append(QuoteIdentifier(parts[i].Trim()));
+		}
+
+		return sb.ToString();
+	}
+
+	private static string QuoteIdentifier(string identifier)
+	{
+		if (string.IsNullOrWhiteSpace(identifier))
+			throw new ArgumentException("Identifier cannot be null or empty.", nameof(identifier));
+
+		var trimmed = identifier.Trim();
+		foreach (var ch in trimmed)
+		{
+			if (char.IsAsciiLetterOrDigit(ch) || ch is '_' or '$' or '#') continue;
+			throw new ArgumentException($"Identifier contains unsupported character '{ch}'.", nameof(identifier));
+		}
+
+		return $"\"{trimmed}\"";
+	}
+
+	private static OracleDbType GetOracleDbTypeFromDotnetType(Type type)
+	{
+		var t = Nullable.GetUnderlyingType(type) ?? type;
+
+		if (t == typeof(byte[])) return OracleDbType.Blob;
+		if (t == typeof(string)) return OracleDbType.Varchar2;
+		if (t == typeof(DateTime)) return OracleDbType.Date;
+		if (t == typeof(decimal)) return OracleDbType.Decimal;
+		if (t == typeof(int)) return OracleDbType.Int32;
+		if (t == typeof(long)) return OracleDbType.Int64;
+		if (t == typeof(short)) return OracleDbType.Int16;
+		if (t == typeof(sbyte) || t == typeof(byte)) return OracleDbType.Byte;
+		if (t == typeof(float)) return OracleDbType.Single;
+		if (t == typeof(double)) return OracleDbType.Double;
+		if (t == typeof(bool)) return OracleDbType.Boolean;
+		if (t == typeof(char)) return OracleDbType.Char;
+
+		return OracleDbType.Varchar2;
+	}
+
+	private void ValidateConnection()
+	{
+		if (_externalTransaction is not null && !ReferenceEquals(_externalTransaction.Connection, _connection))
+			throw new InvalidOperationException("OracleTransaction does not belong to the provided OracleConnection.");
+	}
+
+	private void OpenConnection()
+	{
+		if (_connection.State is ConnectionState.Open) return;
+
+		if (_ownsTheConnection)
+		{
+			_connection.Open();
+			return;
+		}
+
+		throw new InvalidOperationException("The provided OracleConnection must be open before calling WriteToServer.");
+	}
+
+	public void Close()
+	{
+		Dispose();
+	}
+
+	private void ThrowIfDisposed()
+	{
+		ObjectDisposedException.ThrowIf(_disposed, typeof(OracleBulkCopy));
+	}
 }
